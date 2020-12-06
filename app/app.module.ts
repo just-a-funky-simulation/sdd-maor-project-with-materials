400de1b44eac495c58c6530aa86bff5b9c25c563
just-a-funky-simulation <marilena.sayfullina@gmail.com>
2020-12-06 15:50:23 +1100

    3 files modified, 6 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,10 +14,12 @@ import {
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LoginComponent, FilterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
